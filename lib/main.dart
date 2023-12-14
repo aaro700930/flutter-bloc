33053d9c026475a69140896c545a0c64b2513aaa
Alfredo Rivera <aaro700930@gmail.com>
2023-12-13 23:01:49 -0800

    Revert "Terminado video de router"
    This reverts commit 4b1e709b1d74cab53d44eb558f35b85245fbd551.

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:blocs_app/config/config.dart';
+import 'package:blocs_app/presentation/blocs/blocs.dart';
+
+class BlocsProvider extends StatelessWidget {
+  const BlocsProvider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<UsernameCubit>(
+          create: (context) => UsernameCubit(),
+        ),
+      ],
+      child: const MyApp(),
+    );
+  }
+}
+
+void main() => runApp(const BlocsProvider());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Flutter BLoC',
+      debugShowCheckedModeBanner: false,
+      routerConfig: appRouter,
+      theme: AppTheme(isDarkmode: false).getTheme(),
+    );
+  }
+}
